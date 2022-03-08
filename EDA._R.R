8a46c7aa8f91f768a3d1313bc39570dd651fa2a4
Chathuri Jayaweera <chathurijayaweera@Outlook.com>
2022-03-08 09:09:29 +0530

    Adding R EDA script

---
+++ EDA._R.R
@@ -0,0 +1,16 @@
+
+# Importing packages ------------------------------------------------------
+
+library(tidyverse)
+library(ggplot2)
+
+# Reading csv file --------------------------------------------------------
+
+stream_data = read.csv(file='streaming_data.csv')
+summary(stream_data)
+glimpse(stream_data)
+
+
+# Converting date column to seperate year, month, date columns ------------
+
+
